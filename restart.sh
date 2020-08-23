1a7f7fb29caca1356261466cdf32664eaca9e567
Francis Masha <francismasha96@gmail.com>
2020-08-23 20:54:19 +0300

    feat: enhance with certbot

--- restart.sh
+++ restart.sh
@@ -4,7 +4,7 @@ if [ $? -eq 0 ]; then
   echo "Mosquitto is running."
   pkill -f "mosquitto"
   sleep 1
-  /usr/sbin/mosquitto -c /mosquitto/conf/mosquitto.conf &
+  /usr/sbin/mosquitto -c /etc/mosquitto/conf/mosquitto.conf &
 else
   echo "Mosquitto is not running."
 fi
